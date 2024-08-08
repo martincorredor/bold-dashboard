d18e9859687be245b7c8b3f875992626de558899
Martin Corredor <dev.martincorredor@gmail.com>
2024-08-07 19:52:43 -0500

    [UPDATE] Fetching data. Fetch data from endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -6,12 +6,28 @@ import TotalSalesCard from './components/TotalSalesCard/TotalSalesCard';
 import DateFilterBar from './components/DateFilterBar/DateFilterBar';
 
 function App() {
+  const [data, setData] = useState<any[]>([]);
+  console.log('DATA: ', data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://bold-fe-api.vercel.app/api');
+        const result = await response.json();
+        setData(result.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         {/* <TotalSalesCard selectedPeriod="Junio" amount={394.561} /> */}
-        <DateFilterBar handleDateFilter={() => {}} />
+        {/* <DateFilterBar handleDateFilter={() => {}} /> */}
       </div>
     </div>
   );
